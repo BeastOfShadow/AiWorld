cfaeeed55beadad8bcd4f7b190a2e943a84fb9f6
BeastOfShadow <simone.negro.2002@gmail.com>
2025-07-15 19:03:11 +0200

    Correct endpoint
    Co-Authored-By: Daniele-cosimo <32614957+The-Forest03@users.noreply.github.com>

--- aiworld.client/vite.config.js
+++ aiworld.client/vite.config.js
@@ -52,7 +52,7 @@ export default defineConfig({
     },
     server: {
         proxy: {
-            '^/weatherforecast': {
+            '^/api': {
                 target,
                 secure: false
             }
